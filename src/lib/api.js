6531394640b1b8ccdc3b857324aa52b4b2918daa
Cecep Nandang <cecep@kima.one>
2026-05-15 23:19:09 +0700

    fix: bug

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://api-inventory.isavralabel.com/arin-parabola/api",
+  baseURL: "https://api-inventory.isavralabel.com/nyonya-susanti/api",
 });
 
 /** Pesan error dari respons API (axios) untuk toast / UI. */
